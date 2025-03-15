7c0c1dcb6a39fbaa37eb63475026accd45b0bbdd
Sneha Nangunoori <sneha.nangunoori@gmail.com>
2025-03-15 16:53:42 -0500

    Fixed APScore Form Functionality

--- src/APScore/ApScore.js
+++ src/APScore/ApScore.js
@@ -1,46 +1,39 @@
-const readline = require('readline');
+function apScore() {
+    
+    const apClass = document.getElementById("class").value;
+    const score = document.getElementById("score").value;
+        
+    if (score <= 2) {
+        document.getElementById("output").innerHTML = "At this time the University of Texas at Dallas isn't accepting scores of 2 or lower.";
+    }
+    
+    // Hard Coding scores for Subjects
+    
+    if(apClass == "AP Chemistry" && score == 4) {
+        document.getElementById("output").innerHTML = "Congratulations! You will be able to receive credit for " + apClass + ". Credit awarded: CHEM 1311 + Lab";
+    }
+    
+    else if (apClass == "AP Chemistry" && score == 5) {
+        document.getElementById("output").innerHTML = "Congratulations! You will be able to receive credit for " + apClass + ". Credit awarded: CHEM 1311 + Lab";
+    }
+    
+    else if (apClass == "AP Chemistry" && score == 3) {
+        document.getElementById("output").innerHTML = "Credit awarded: 3 SCH free electives";
+    }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Please enter the AP classes you took in high school: ", function(apClasses) {
-    rl.question("Please enter the score you received: ", function(scoreInput) {
-        const score = parseInt(scoreInput);
+    
+    if(apClass == "AP Biology" && score == 4) {
+        document.getElementById("output").innerHTML = "Congratulations! You will be able to receive credit for " + apClass + ". Credit awarded: BIOL 2312 + 2112, BIOL 2311 + 2111";
+    }
+    
+    else if(apClass == "AP Biology" && score == 5) {
+        document.getElementById("output").innerHTML = "Congratulations! You will be able to receive credit for " + apClass + ". Credit awarded: BIOL 2312 + 2112, BIOL 2311 + 2111";
+    }
+    
+    else if (apClass == "AP Biology" && score == 3) {
+        document.getElementById("output").innerHTML = "Credit awarded: 3 SCH free electives";
+    }
+    
+    // Repeat the same pattern for other subjects...
+}
         
-        if (score <= 2) {
-            console.log("At this time the University of Texas at Dallas isn't accepting scores of 2 or lower.");
-        }
-        
-        // Hard Coding scores for subjects
-        
-        if(apClasses === "AP Chemistry" && score === 4) {
-            console.log("Congratulations! You will be able to receive credit for " + apClasses + ". Credit awarded: CHEM 1311† + Lab");
-        }
-        
-        else if (apClasses === "AP Chemistry" && score === 5) {
-            console.log("Congratulations! You will be able to receive credit for " + apClasses + ". Credit awarded: CHEM 1311† + Lab");
-        }
-        
-        else if (score === 3) {
-            console.log("Credit awarded: 3 SCH free electives");
-        }
-        
-        if(apClasses === "AP Biology" && score === 4) {
-            console.log("Congratulations! You will be able to receive credit for " + apClasses + ". Credit awarded: BIOL 2312† + 2112, BIOL 2311† + 2111");
-        }
-        
-        else if(apClasses === "AP Biology" && score === 5) {
-            console.log("Congratulations! You will be able to receive credit for " + apClasses + ". Credit awarded: BIOL 2312† + 2112, BIOL 2311† + 2111");
-        }
-        
-        else if (score === 3) {
-            console.log("Credit awarded: 3 SCH free electives");
-        }
-        
-        // Repeat the same pattern for other subjects...
-        
-        rl.close();
-    });
-});
